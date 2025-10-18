d0696f332e24bd1f548b63cc20fa04b20f8d19fd
Tatiana Andreeva <snurnisyan@gmail.com>
2025-10-18 16:55:44 +0200

    fix: fix .env routes

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'git@github.com:snurnisyan/nodejs-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp -i ${DEPLOY_SSH_KEY} ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/nodejs-pm2-deploy/backend/`,
+      'pre-deploy-local': `scp -i ${DEPLOY_SSH_KEY} ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/nodejs-pm2-deploy/backend/`,
       'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
